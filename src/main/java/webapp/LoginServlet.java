cac16f863665ec1a35652c9eb60c399c27641b7f
darshan16011989 <98390319+darshan16011989@users.noreply.github.com>
2026-06-16 07:26:19 +0100

    Update LoginServlet.java

--- src/main/java/webapp/LoginServlet.java
+++ src/main/java/webapp/LoginServlet.java
@@ -43,6 +43,7 @@ public class LoginServlet extends HttpServlet {
 		out.println("<body>");
 		out.println("<h1> Welcome to Devops Training</h1>");
 		out.println("<h1> This is fully automated pipeline</h1>");
+		out.println("<h1> With new features</h1>");
 
 		
 		
